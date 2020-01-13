488bc11a84b11208c279c70a379e8f64925ad975
mdraza <raza.uidev@gmail.com>
2020-01-13 12:58:09 +0530

    initial commit

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@
 $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip(); 
 });
+
+/****  Preloader  ****/
+// $("#status").fadeOut();
+$("#preloader")
+  .delay(550)
+  .fadeOut("slow");
+$("body")
+  .delay(550)
+  .css({ overflow: "visible" });
